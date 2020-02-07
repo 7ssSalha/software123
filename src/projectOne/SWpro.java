8694227dab253261c8a32f031feafd603d9a3e8e
7ssSalha <husseinsoheil1999@hotmail.com>
2020-02-07 13:29:22 +0200

    1st com

---
+++ src/projectOne/SWpro.java
@@ -0,0 +1,14 @@
+package projectOne;
+
+public class SWpro {
+
+	public static void print(String a) {
+		System.out.print(a);;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		print("hussein");
+	}
+
+}
